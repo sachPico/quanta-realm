c4df9ff1879e8c373d21539ac5fa82eda5d4a217
Khalil <khalilandaal@gmail.com>
2020-09-04 08:38:49 +0700

    Added object pool mechanism

--- Assets/Scripts/MaterialBehaviour.cs
+++ Assets/Scripts/MaterialBehaviour.cs
@@ -16,21 +16,17 @@ public class MaterialBehaviour : MonoBehaviour
     RaycastHit rayHitInfo = new RaycastHit();
     ComputeBuffer matBuffer;
 
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     [ExecuteInEditMode]
     void Update()
     {
         //hitInfos = new Vector4[numOfRayCasts];
-        for(int i=0; i<numOfRayCasts; i++)
+        for (int i = 0; i < numOfRayCasts; i++)
         {
             hitInfos[i] = Vector4.zero;
-            Vector3 startPoint = -transform.right*5f + transform.position - (transform.forward*laserWidth/2) + (transform.forward*(2*i+1)*laserWidth/(2*numOfRayCasts));
+            Vector3 startPoint = -transform.right * 5f + transform.position - (transform.forward * laserWidth / 2) + (transform.forward * (2 * i + 1) * laserWidth / (2 * numOfRayCasts));
             laserRays = new Ray(startPoint, transform.right);
-            if(Physics.Raycast(laserRays, out rayHitInfo))
+            if (Physics.Raycast(laserRays, out rayHitInfo))
             {
                 hitInfos[i] = new Vector4(rayHitInfo.point.x, rayHitInfo.point.y, rayHitInfo.point.z, 1);
             }
@@ -41,35 +37,29 @@ public class MaterialBehaviour : MonoBehaviour
             // hitInfos[i] = tmpInfo;
         }
 
-        matBuffer = new ComputeBuffer(hitInfos.Length, sizeof(float)*4);
-        Graphics.ClearRandomWriteTargets();
-        _mat.SetPass(0);
-        _mat.SetBuffer("hitInfoBuffer", matBuffer);
-        Graphics.SetRandomWriteTarget(1, matBuffer, false);
-        _mat.SetVector("_BottomLimit",transform.position - new Vector3(5,5,0));
-        _mat.SetVector("_LaserForwardDirection",transform.right.normalized);
-        _mat.SetVector("_LaserUpDirection",transform.forward.normalized);
-        _mat.SetVector("_FirstHitInfo",hitInfos[0]);
+        matBuffer = new ComputeBuffer(hitInfos.Length, sizeof(float) * 4);
+        //Graphics.ClearRandomWriteTargets();
+        //_mat.SetPass(0);
+        //_mat.SetBuffer("hitInfoBuffer", matBuffer);
+        //Graphics.SetRandomWriteTarget(1, matBuffer, false);
+        _mat.SetVector("_BottomLimit", transform.position - new Vector3(5, 5, 0));
+        _mat.SetVector("_LaserForwardDirection", transform.right.normalized);
+        _mat.SetVector("_LaserUpDirection", transform.forward.normalized);
+        _mat.SetVector("_FirstHitInfo", hitInfos[0]);
         _mat.SetVectorArray("_HitInfos", hitInfos);
         _mat.SetFloat("laserWidth", laserWidth);
         _mat.SetInt("numOfRayCasts", numOfRayCasts);
 
         matBuffer.Dispose();
-
-        //_mat.SetBuffer("_hitPosInfo", _shaderHitInfo);
-        // if(isCopyFromCompBuffer)
-        // {
-        //     _shaderHitInfo.GetData(copyHitInfos);
-        // }
     }
 
     void OnDrawGizmos()
     {
-        for(int i=0; i<numOfRayCasts; i++)
+        for (int i = 0; i < numOfRayCasts; i++)
         {
-            Vector3 startPoint = -transform.right*5f + transform.position - (transform.forward*laserWidth/2) + (transform.forward*(2*i+1)*laserWidth/(2*numOfRayCasts));
+            Vector3 startPoint = -transform.right * 5f + transform.position - (transform.forward * laserWidth / 2) + (transform.forward * (2 * i + 1) * laserWidth / (2 * numOfRayCasts));
             gLaserRays = new Ray(startPoint, transform.right);
-            if(Physics.Raycast(gLaserRays, out rayHitInfo))
+            if (Physics.Raycast(gLaserRays, out rayHitInfo))
             {
                 Gizmos.color = Color.red;
                 Gizmos.DrawLine(gLaserRays.origin, rayHitInfo.point);
@@ -77,13 +67,8 @@ public class MaterialBehaviour : MonoBehaviour
             else
             {
                 Gizmos.color = Color.green;
-                Gizmos.DrawLine(gLaserRays.origin, gLaserRays.origin + (transform.right*500f));
+                Gizmos.DrawLine(gLaserRays.origin, gLaserRays.origin + (transform.right * 500f));
             }
         }
-    }
-
-    void OnDestroy()
-    {
-        matBuffer.Dispose();
     }
 }
---
+++ Assets/Scripts/Utility/SHMUPObjectPool/SHMUPObjectPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    HOW TO USE
+ */
+
+public class SHMUPObjectPool : MonoBehaviour
+{
+    [System.Serializable]
+    public struct ObjectProperty
+    {
+        public string objTag;
+        public GameObject objPrefab;
+        public int defaultSpawnNumber;
+    }
+
+    public Transform playfieldTransform;
+    public Dictionary<string, List<GameObject>> objectPool = new Dictionary<string, List<GameObject>>();
+
+    [SerializeField]
+    public List<ObjectProperty> objProp;
+
+    public void Start()
+    {
+        List<GameObject> tmpSpawnList = new List<GameObject>();
+        GameObject tmpSpawnObj;
+        for (int i = 0; i < objProp.Count; i++)
+        {
+            for (int j = 0; j < objProp[i].defaultSpawnNumber; j++)
+            {
+                tmpSpawnObj = Instantiate(objProp[i].objPrefab, Vector3.zero, Quaternion.identity);
+                tmpSpawnObj.SetActive(false);
+                tmpSpawnList.Add(tmpSpawnObj);
+                //Debug.Log("tmpSpawnList's size is " + tmpSpawnList.Count);
+            }
+            objectPool.Add(objProp[i].objTag, tmpSpawnList);
+        }
+
+        for (int i = 0; i < objectPool.Count; i++)
+        {
+            Debug.Log("Pool tagged "+objProp[i].objTag+" has "+objectPool[objProp[i].objTag].Count+" objects.");
+        }
+    }
+}
