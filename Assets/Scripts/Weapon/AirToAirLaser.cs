544deba154c5574d40b810751dfe318aeb2e4808
Khalil Satyadama <khalilandaal@gmail.com>
2021-04-27 23:26:24 +0700

    [TEST] Test old AirToAirLaser script

--- Assets/Scripts/Weapon/AirToAirLaser.cs
+++ Assets/Scripts/Weapon/AirToAirLaser.cs
@@ -4,71 +4,59 @@ using UnityEngine;
 
 public class AirToAirLaser : MonoBehaviour
 {
-    [Range(0,45)]
-    public int details;
-    public float radius;
+    public float squaredRadius;
     public GameObject redLaser;
     public GameObject blueLaser;
     public GameObject debug;
 
-    private int index = 0;
-    private int iterator = 1;
-    private Vector3 blueLaserPos;
-    public float[] direction;
-    public Vector3[] offset;
+    private float center;
 
-    private void Precompute()
+    private float sqrDist;
+    private float radius;
+    private float offset;
+    private float multiplier = 1;
+    private ParticleSystem particleSystem;
+    private Vector3 redLocalPos;
+    private Vector3 blueLocalPos;
+    private Vector3 initialLocalPos;
+
+    private void OnEnable()
     {
-        float initialDirection;
-        float interval;
-        float offsetDist;
-        float tmp;
-
-        interval = 180f / details;
-        initialDirection = (180f - interval) / 2f;
-
-        offsetDist = GetOffsetDist(interval);
-
-        offset = new Vector3[details];
-        direction = new float[details];
-        for (int i = 0; i < details; i++)
-        {
-            direction[i] = Mathf.Deg2Rad * (initialDirection - interval * i);
-            offset[i] = new Vector3(Mathf.Cos(direction[i]), Mathf.Sin(direction[i]), 0) * offsetDist;// * Time.fixedDeltaTime;
-        }
-    }
-
-    private float GetOffsetDist(float intervalRad)
-    {
-        Vector3 a, b;
-        a = new Vector3(Mathf.Cos(0), Mathf.Sin(0), 0) * radius;
-        b = new Vector3(Mathf.Cos(intervalRad), Mathf.Sin(intervalRad), 0) * radius;
-        return Vector3.Distance(a,b);
+        squaredRadius = 64;
+        radius = Mathf.Sqrt(squaredRadius);
+        offset = radius;
+        debug.transform.localPosition = new Vector3(offset, 0f, 0f);
+        //Debug.Break();
     }
 
     private void Start()
     {
-        Precompute();
-    }
-
-    private void OnEnable()
-    {
-        redLaser.transform.localPosition = Vector3.zero;
-        blueLaser.transform.localPosition = Vector3.zero;
+        //particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     private void FixedUpdate()
     {
-        redLaser.transform.localPosition += offset[index];
-        blueLaserPos = redLaser.transform.localPosition;
-        blueLaserPos.y *= -1;
-        blueLaser.transform.localPosition = blueLaserPos;
-
-        index += iterator;
-        if(index >= details -1 || index <= 0)
+        sqrDist = Vector3.SqrMagnitude(transform.localPosition - debug.transform.localPosition);
+        if (sqrDist >= squaredRadius)
         {
-            iterator *= -1;
+            squaredRadius = 16;
+            offset+=radius;
+            radius = Mathf.Sqrt(squaredRadius);
+            offset+=radius;
+            debug.transform.localPosition = new Vector3(offset, 0f, 0f);
+            multiplier *= -1;
+            // Debug.Break();
         }
+
+        sqrDist = Vector3.SqrMagnitude(transform.localPosition - debug.transform.localPosition);;
+        redLocalPos.y = (Mathf.Sqrt(Mathf.Abs(squaredRadius - sqrDist))) * multiplier;
+        blueLocalPos.y = -redLocalPos.y;
+
+        redLaser.transform.localPosition = redLocalPos;
+        blueLaser.transform.localPosition = blueLocalPos;
     }
 
+    private void OnDrawGizmos()
+    {
+    }
 }
