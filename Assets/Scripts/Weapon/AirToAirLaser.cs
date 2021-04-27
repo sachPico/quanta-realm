f0a1d0581c662195a80d8de843d2a800fd128534
Khalil <khalilandaal@gmail.com>
2021-04-27 16:54:43 +0700

    [UPDATE] Updated AirToAir movement behaviour

--- Assets/Scripts/Weapon/AirToAirLaser.cs
+++ Assets/Scripts/Weapon/AirToAirLaser.cs
@@ -4,50 +4,71 @@ using UnityEngine;
 
 public class AirToAirLaser : MonoBehaviour
 {
-    public float squaredRadius;
+    [Range(0,45)]
+    public int details;
+    public float radius;
     public GameObject redLaser;
     public GameObject blueLaser;
     public GameObject debug;
 
-    private float center;
+    private int index = 0;
+    private int iterator = 1;
+    private Vector3 blueLaserPos;
+    public float[] direction;
+    public Vector3[] offset;
 
-    private float radius;
-    private Vector3 redLocalPos;
-    private Vector3 blueLocalPos;
-    private Vector3 initialLocalPos;
+    private void Precompute()
+    {
+        float initialDirection;
+        float interval;
+        float offsetDist;
+        float tmp;
+
+        interval = 180f / details;
+        initialDirection = (180f - interval) / 2f;
+
+        offsetDist = GetOffsetDist(interval);
+
+        offset = new Vector3[details];
+        direction = new float[details];
+        for (int i = 0; i < details; i++)
+        {
+            direction[i] = Mathf.Deg2Rad * (initialDirection - interval * i);
+            offset[i] = new Vector3(Mathf.Cos(direction[i]), Mathf.Sin(direction[i]), 0) * offsetDist;// * Time.fixedDeltaTime;
+        }
+    }
+
+    private float GetOffsetDist(float intervalRad)
+    {
+        Vector3 a, b;
+        a = new Vector3(Mathf.Cos(0), Mathf.Sin(0), 0) * radius;
+        b = new Vector3(Mathf.Cos(intervalRad), Mathf.Sin(intervalRad), 0) * radius;
+        return Vector3.Distance(a,b);
+    }
+
+    private void Start()
+    {
+        Precompute();
+    }
 
     private void OnEnable()
     {
-        squaredRadius = 64;
-        radius = Mathf.Sqrt(squaredRadius);
-        center = transform.localPosition.x + radius;
-        initialLocalPos = transform.localPosition;
-        //Debug.Break();
+        redLaser.transform.localPosition = Vector3.zero;
+        blueLaser.transform.localPosition = Vector3.zero;
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(transform.localPosition.x - center+":"+radius);
-        if (transform.localPosition.x - center >= radius)
+        redLaser.transform.localPosition += offset[index];
+        blueLaserPos = redLaser.transform.localPosition;
+        blueLaserPos.y *= -1;
+        blueLaser.transform.localPosition = blueLaserPos;
+
+        index += iterator;
+        if(index >= details -1 || index <= 0)
         {
-            squaredRadius = 16;
-            radius = Mathf.Sqrt(squaredRadius);
-            center = transform.localPosition.x + radius;
-            initialLocalPos = transform.localPosition;
-            Debug.Log("Hubla");
-            //Debug.Break();
+            iterator *= -1;
         }
-
-        redLocalPos.y = Mathf.Sqrt(Mathf.Abs(squaredRadius - Mathf.Pow(Mathf.Abs(transform.localPosition.x - center),2)));
-        //Debug.Log(redLocalPos.y);
-        blueLocalPos.y = -redLocalPos.y;
-
-        redLaser.transform.localPosition = redLocalPos;
-        blueLaser.transform.localPosition = blueLocalPos;
-        debug.transform.position = transform.parent.TransformPoint(initialLocalPos+Vector3.right*radius);// - new Vector3(center,0,0));
     }
 
-    private void OnDrawGizmos()
-    {
-    }
 }
