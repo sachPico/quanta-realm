069fec12ea1c847774596781663b0e77808a43e8
Khalil <khalilandaal@gmail.com>
2021-05-27 19:46:07 +0700

    [WIP] Dynamic Inspector for Stage Settings

---
+++ Assets/Scripts/EnemyProperties.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[System.Serializable]
+public class EnemyProperties<T>
+{
+    private T _data;
+
+    public T data
+    {
+        get { return _data; }
+        set
+        {
+            _data = value;
+            Debug.Log("Type: " + _data.GetType().ToString());
+            switch(_data.GetType().ToString())
+            {
+                case ("PK001"):
+                    PK001 a = _data as PK001;
+                    Debug.Log(a.hoopla);
+                    break;
+                case ("PK002"):
+                    PK002 b = _data as PK002;
+                    Debug.Log(b.hubla);
+                    break;
+            }
+        }
+    }
+
+    public float s;
+}
+
+[Serializable]
+public class Ene
+{
+
+}
+
+[Serializable]
+public class PK001 : Ene
+{
+    public float hoopla;
+
+    public PK001()
+    {
+        hoopla = 4;
+    }
+}
+
+[Serializable]
+public class PK002 : Ene
+{
+    public float hubla;
+
+    public PK002()
+    {
+        hubla = 5;
+    }
+}
---
+++ Assets/Scripts/StageSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Enemy { PK001, PK002 };
+
+[Serializable]
+public class StageEnemyProperties
+{
+    public Enemy enemy;
+    public EnemyProperties<Ene> someObject;
+}
+
+[CreateAssetMenu(fileName = "stage_", menuName = "Stage")]
+public class StageSettings : ScriptableObject
+{
+    public string stageName;
+
+    public StageEnemyProperties[] stageEnemyProperties;
+
+    private void OnValidate()
+    {
+        for (int i = 0; i < stageEnemyProperties.Length; i++)
+        {
+            switch (stageEnemyProperties[i].enemy)
+            {
+                case (Enemy.PK001): stageEnemyProperties[i].someObject.data = new PK001(); break;
+
+                case (Enemy.PK002): stageEnemyProperties[i].someObject.data = new PK002(); break;
+            }
+        }
+    }
+}
